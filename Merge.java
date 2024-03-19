329bfc4284743f84efe06c0b601a47404d37657a
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-03-19 07:58:15 -0400

    Create Merge.java

---
+++ Merge.java
@@ -0,0 +1,19 @@
+public class Merge {
+  
+  public static int[] mergeSort(int[] data){
+    
+  }
+  
+  /* Use any of the merge methods we mentioned in class
+  
+  public static int[] merge(int [] left, int[] right){
+  
+  }
+  
+  public static void merge(int[] destination, int [] left, int[]
+  right){
+  
+  }
+  */
+
+}
